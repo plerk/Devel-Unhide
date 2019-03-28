69ac9a55391f5355143e3349179662d230796dac
Graham Ollis <plicease@cpan.org>
2019-03-28 09:16:12 -0400

    Initial commit

---
+++ lib/Devel/Unhide.pm
@@ -0,0 +1,10 @@
+package Devel::Unhide;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Forces the availability of specified Perl modules that have been forced to be unavailable (for testing)
+# VERSION
+
+1;
